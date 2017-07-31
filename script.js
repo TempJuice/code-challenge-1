7345a970f8ac2afa8a4fc1b81a102803aa4f5c5c
Ryan Templin <tempjuice@hotmail.com>
2017-07-31 14:33:14 -0500

    Final push. Couldn't get jQuery selectors to reference HTML elements created inside an event handler.

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ var counter = 0;
 //Event listener
 $('button').on('click', function () {
     counter = counter + 1;
-console.log(counter);
     //Creating #firstDiv
     $('body').append('<div id ="firstDiv">' + 
     '<p id ="counterParagraph">' + '</p>' + '</div>');
-    console.log(counter);
+    $('#counterParagraph').html('<p>' + counter + '</p>');
+    $('body #firstDiv').append('<button>' + 'Swap' + '</button>' + '<button id = "delete">' + 'Delete' + '</button>')
+  
 });
 
+$('#delete').on('click', function () {
+              $(this).parent().parent().parent().remove();  
+        });
 
 
-$('#counterParagraph').html('<p>' + counter + '</p>');
+//$('body #firstDiv').append('Cant' get my jQuery selectors to do shit when referencing an element that was created after my event handler. So I just tried to get em to work with every click.');
+
 
 });
