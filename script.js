28cd0c4627c42b19e5568cd124022947ce9f05c1
Ryan Templin <tempjuice@hotmail.com>
2017-07-31 13:59:21 -0500

    Successfully creating div and p elements.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(document).ready( function () {
-$('div').append('<button>Generate</button>');
+//Button Counter
+var counter = 0;
+
+//Event listener
+$('button').on('click', function () {
+    counter = counter + 1;
+console.log(counter);
+    //Creating #firstDiv
+    $('body').append('<div id ="firstDiv">' + 
+    '<p id ="counterParagraph">' + '</p>' + '</div>');
+    console.log(counter);
+});
+
+
+
+$('#counterParagraph').html('<p>' + counter + '</p>');
+
 });
